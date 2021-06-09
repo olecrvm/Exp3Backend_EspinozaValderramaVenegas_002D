3b8c04d81582c9a426d7de52d63847c5e2ba1122
olecrvm <n.venegast@duocuc.cl>
2021-06-08 23:41:41 -0400

    se modifico models y se agregaron archivos

--- Exp3/MediPets/admin.py
+++ Exp3/MediPets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TipoMascota,Mascota
 
 # Register your models here.
+admin.site.register(TipoMascota)
+admin.site.register(Mascota)
--- Exp3/MediPets/models.py
+++ Exp3/MediPets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoMascota(models.Model):
+    idTipo=models.IntegerField(primary_key=True, verbose_name='Id de Tipo de Mascota: ')
+    nomTipo=models.CharField(max_length=50, verbose_name='Nombre del Tipo de Mascota: ')
+
+    def __str__(self):
+        return(self.nomTipo)
+
+class Mascota(models.Model):
+    nroChip=models.CharField(primary_key=True, max_length=6, verbose_name='Numero de Chip: ')
+    nombre=models.CharField(max_length=30, verbose_name='Nombre de la Mascota: ')
+    dueno=models.CharField(max_length=30, verbose_name='Nombre del dueño de la Mascota: ')
+    tipo=models.ForeignKey(TipoMascota, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(self.nroChip)
